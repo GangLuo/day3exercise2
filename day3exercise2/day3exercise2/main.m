0ab3a24fe5b245812d4cd139f8aade2875d2a182
GangLuo <luogang1988@gmail.com>
2014-05-07 16:54:35 -0400

    exercise2

---
+++ day3exercise2/day3exercise2/main.m
@@ -0,0 +1,25 @@
+//
+//  main.m
+//  day3exercise2
+//
+//  Created by User1 on 2014-05-07.
+//  Copyright (c) 2014 Tolulope. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{// I think the result is d
+
+    @autoreleasepool {
+        
+        char c ,d ;
+        c = 'd' ;
+        d = c ;
+        NSLog(@"d = %c",d );
+        
+        
+    }
+    return 0;
+}
+
